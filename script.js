fab662cbfac99e445a138d20f9e26991cbcad6de
BSTR65 <161294572+BSTR65@users.noreply.github.com>
2024-02-26 06:23:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-const mensajesAmorCatita = [
-  "Catita, tus abrazos son mi refugio y tu sonrisa ilumina mi día.",
-  "Catita, cada momento contigo es un regalo del cielo que atesoro con todo mi ser.",
-  "Catita, contigo descubrí lo que significa amar de verdad y cada día te amo más.",
-  "Catita, eres mi musa, mi inspiración, mi razón para ser mejor cada día.",
-  "Catita, no hay palabras suficientes para expresar lo mucho que te amo y te admiro.",
-  "Catita, contigo encontré un amor que trasciende el tiempo y el espacio."
-];
+window.onload = function() {
+  // Mostrar el mensaje inicial al cargar la página
+  var mensajeInicial = document.getElementById("mensajeInicial");
+  mensajeInicial.style.display = "block";
+
+  // Ocultar el mensaje inicial después de 5 segundos
+  setTimeout(function() {
+    mensajeInicial.style.display = "none";
+  }, 5000);
+}
 
 function mostrarMensaje() {
-  const indice = Math.floor(Math.random() * mensajesAmorCatita.length);
-  alert(mensajesAmorCatita[indice]);
+    // Mostrar las opciones "Sí" y "No"
+    var opcion = confirm("¿Me amas?");
+    
+    // Si la opción es "Sí", mostrar los mensajes de amor
+    if (opcion) {
+        const mensajesAmor = [
+            "Catita, tus abrazos son mi refugio y tu sonrisa ilumina mi día.",
+            "Catita, cada momento contigo es un regalo del cielo que atesoro con todo mi ser.",
+            "Catita, contigo descubrí lo que significa amar de verdad y cada día te amo más.",
+            "Catita, eres mi musa, mi inspiración, mi razón para ser mejor cada día.",
+            "Catita, no hay palabras suficientes para expresar lo mucho que te amo y te admiro.",
+            "Catita, contigo encontré un amor que trasciende el tiempo y el espacio."
+        ];
+        
+        const indice = Math.floor(Math.random() * mensajesAmor.length);
+        alert(mensajesAmor[indice]);
+    } else { // Si la opción es "No", mostrar el emoticono triste
+        alert(":( ");
+    }
 }
